f1177c646e832aedb8b43d48f29c63755afc9340
sn1638 <sachin111mrj@gmail.com>
2025-10-26 01:47:47 +0530

    border improved

--- src/second.jsx
+++ src/second.jsx
@@ -5,7 +5,7 @@ export default function Second() {
       <div className="border border-gray-600 overflow-x-hidden w-full mx-auto bg-black sm:block md:flex lg:flex gap-4 items-center justify-center  pt-10 h-full pb-20">
         <div className="w-full lg:w-1/2">
           <motion.div
-            className="border border-gray-700 rounded-2xl bg-gray-900 italic text-center text-white font-thin text-6xl pt-10 pb-20"
+            className="border border-b-0 border-gray-700 rounded-2xl rounded-b-none bg-gray-900 italic text-center text-white font-thin text-6xl pt-10 pb-20"
             initial={{ x: '-100vw',opacity:0 }}
              animate={{ x: 0 ,opacity:1}}
             transition={{ duration:1, repeat: 0, ease: 'easeInOut' }}
@@ -13,7 +13,7 @@ export default function Second() {
             Sachin kumar verma
           </motion.div>
           <motion.div
-            className="border border-gray-700 pb-1 rounded-xl bg-gray-900 text-center text-3xl text-white"
+            className="border border-gray-700 border-t-0 border-b-0 pb-1 bg-gray-900 text-center text-3xl text-white"
              initial={{ x: '-100vw',opacity:0 }}
              animate={{ x: 0,opacity:1 }}
             transition={{ duration: 1.5, repeat: 0, ease: 'easeInOut' }}
@@ -21,7 +21,7 @@ export default function Second() {
             Full stack Developer
           </motion.div>
           <motion.p
-            className="bg-gray-900 border border-gray-700 rounded-2xl w-full text-center text-white p-4 py-6 text-thin text-xl sm:w-full"
+            className="bg-gray-900 border-t-0 rounded-t-none border border-gray-700 rounded-2xl w-full text-center text-white p-4 py-6 text-thin text-xl sm:w-full"
              initial={{ x: '-100vw',opacity:0 }}
              animate={{ x: 0,opacity:1 }}
             transition={{ duration: 2, repeat: 0, ease: 'easeInOut' }}
